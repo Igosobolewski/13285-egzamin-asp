49bf610f5d361f7ddaeea30a9bfcbcc5de7b5729
Igosobolewski <igosobolewski@gmail.com>
2022-02-11 11:23:12 +0100

    Dodaj pliki projektów.

---
+++ 13285-egzamin-asp/Controllers/BookController.cs
@@ -0,0 +1,49 @@
+﻿using _13285_egzamin_asp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _13285_egzamin_asp.Controllers
+{
+    [Route("api/book")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private static List<Book> books = new List<Book>()
+        {
+            new Book(){Id=1, Title="AAAA", Author="BBBB", Price=100},
+            new Book(){Id=2, Title="CCCC", Author="DDDD", Price=50},
+            new Book(){Id=3, Title="EEEE", Author="FFFF", Price=10}
+
+        };
+        
+        [HttpGet]
+        public IEnumerable<Book> Get()
+        {
+            return books;
+        }
+
+
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var zmienna = books.Where(x => x.Id.Equals(id)).First();
+            if (zmienna != null)
+            {
+                int index = id - 1;
+                books.RemoveAt(index);
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+
+        }
+
+
+    }
+
+}
+
---
+++ 13285-egzamin-asp/Models/Book.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace _13285_egzamin_asp.Models
+{
+    public class Book
+    {
+        [HiddenInput]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Musisz podać tytuł książki!")]
+        [MinLength(3, ErrorMessage = "Tytuł nie może być krótszy niż 3 znaki")]
+        [Display(Name = "Tytuł książki")]
+        [MaxLength(200, ErrorMessage = "Tytuł nie może być dłuższa niż 200 znaków")]
+        public string Title { get; set; }
+
+        [MinLength(3, ErrorMessage = "Imię nie może być krótsze niż 3 znaków")]
+        [MaxLength(200, ErrorMessage = "Imię nie może być dłuższe niż 200 znaków")]
+        [Required(ErrorMessage = "Musisz podać autora!")]
+        public string Author { get; set; }
+
+        [Required(ErrorMessage = "Musisz podać cene książki!")]
+        [Range(minimum: 0, maximum: 9999, ErrorMessage = "Cena nie może być ujemna ani większa niż 9999!")]
+        [Column(TypeName = "decimal(8, 2)")]
+        public decimal Price { get; set; }
+    }
+}
